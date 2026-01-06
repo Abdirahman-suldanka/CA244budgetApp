5a5942b371f347ba4844c9523c8e72a9d0182f83
abdirahman-suldanka <abdurahmansuldanka@gmail.com>
2026-01-06 09:25:09 +0300

    version 1

---
+++ main.js
@@ -0,0 +1,78 @@
+const budgetForm = document.getElementById("budgetForm");
+const budgetInput = document.getElementById("budgetInput");
+const totalBudget = document.getElementById("totalBudget");
+const remainingBudget = document.getElementById("remainingBudget");
+
+const expForm = document.getElementById("expenseForm");
+const expName = document.getElementById("expenseName");
+const expAmount = document.getElementById("expenseAmount");
+const expList = document.getElementById("expenseList");
+const amountError = document.getElementById("alert");
+
+budget = 0;
+remaining = 0;
+
+function saveToLocal() {
+  localStorage.setItem("budget", budget);
+  localStorage.setItem("remaining", remaining);
+  localStorage.setItem("expenses", expList.innerHTML);
+}
+
+budgetForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  budget = budgetInput.value;
+  remaining = budget;
+
+  totalBudget.innerHTML = budget;
+  remainingBudget.innerHTML = remaining;
+  saveToLocal();
+  budgetInput.value = "";
+  expList.innerHTML = "";
+});
+
+expForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  name = expName.value;
+  amount = expAmount.value;
+
+  if (amount > remaining) {
+    amountError.classList.remove("hidden");
+    return;
+  }
+
+  amountError.classList.add("hidden");
+
+  remaining -= amount;
+  remainingBudget.innerHTML = remaining;
+
+  displayDiv = document.createElement("div");
+  displayDiv.innerHTML = `
+    <span> ${name} </span>
+    <span>  ${amount} </span>
+  `;
+  displayDiv.className =
+    "flex justify-between border border-red-300 bg-red-50 p-3 rounded-lg";
+
+  expList.appendChild(displayDiv);
+  saveToLocal();
+  expName.value = "";
+  expAmount.value = "";
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  savedBudget = localStorage.getItem("budget");
+  savedRemaining = localStorage.getItem("remaining");
+  savedExpenses = localStorage.getItem("expenses");
+  if (savedBudget) {
+    budget = savedBudget;
+    totalBudget.innerHTML = budget;
+  }
+
+  if (savedRemaining) {
+    remaining = savedRemaining;
+    remainingBudget.innerHTML = remaining;
+  }
+  if (savedExpenses) {
+    expList.innerHTML = savedExpenses;
+  }
+});
